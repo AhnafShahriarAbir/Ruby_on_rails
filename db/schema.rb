746461ec83d339a3e475dfd43b80b5b2a7b503ed
AhnafShahriarAbir <s3577189@student.rmit.edu.au>
2019-05-03 01:56:34 +1000

    sign up changes

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20190502131821) do
 
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "courses"
@@ -34,12 +35,20 @@ ActiveRecord::Schema.define(version: 20190502131821) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "locations", force: :cascade do |t|
+    t.string "place"
+    t.integer "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
