0748792c835a46687856e14f44e26db7fffcf2c2
Danny Van <s3717353@student.rmit.edu.au>
2019-05-03 15:12:37 +1000

    courses framework, error message to non RMIT emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
-  validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
+  validates :name,  presence: true, length: { minimum: 1}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[rmit.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { minimum: 4 },
+                    format: { with: VALID_EMAIL_REGEX, message: 'registration must be from an RMIT personal' },
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
