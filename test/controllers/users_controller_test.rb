77597efb3ee592b23a5ecb5dc8033335da42f299
AhnafShahriarAbir <s3577189@student.rmit.edu.au>
2019-05-15 08:54:44 +1000

    user admin added

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -22,4 +22,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                             admin: FILL_IN } }
     assert_not @other_user.FILL_IN.admin?
   end
+  
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
 end
